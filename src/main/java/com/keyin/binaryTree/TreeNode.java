fbfc2e5f9a7c594f5f5c9dce9c7455879786360b
Noah <Noah.Whiffen@keyin.com>
2025-08-06 12:40:26 -0230

    Node class added

--- src/main/java/com/keyin/binaryTree/TreeNode.java
+++ src/main/java/com/keyin/binaryTree/TreeNode.java
@@ -1,4 +1,45 @@
 package com.keyin.binaryTree;
 
 public class TreeNode {
+    private int value;
+    private TreeNode left;
+    private TreeNode right;
+    private int height;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.height = 1;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
